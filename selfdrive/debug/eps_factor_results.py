73301b1def998f136bf2525e6c6359ff0f5c2970
Shane Smiskol <shane@smiskol.com>
2020-10-22 14:44:54 -0500

    add data to plot eps finder results

--- selfdrive/debug/eps_factor_results.py
+++ selfdrive/debug/eps_factor_results.py
@@ -1,3 +1,4 @@
+import numpy as np
 import matplotlib.pyplot as plt
 
 data = [{'miles': 9.58825, 'factor': 0.8859250863366187}, {'miles': 6.48633, 'factor': 0.8781094350554707}, {'miles': 19.3983, 'factor': 0.9758311448751528},
@@ -7,10 +8,25 @@ data = [{'miles': 9.58825, 'factor': 0.8859250863366187}, {'miles': 6.48633, 'fa
         {'miles': 3.88626, 'factor': 0.8639352841524472}, {'miles': 3.84592, 'factor': 0.9199012714741803}, {'miles': 3.83296, 'factor': 0.8754803559581712},
         {'miles': 75.0482, 'factor': 0.8776047319911336}]
 
+data_filtered = [{'miles': 74.4485, 'factor': 0.9398601268802427}, {'miles': 3.88626, 'factor': 0.8727060958492365}, {'miles': 3.9699, 'factor': 0.9251274335155091},
+                 {'miles': 75.0482, 'factor': 0.8803644959678146}, {'miles': 14.2211, 'factor': 0.9043003173790769}, {'miles': 9.58825, 'factor': 0.8965687068323793},
+                 {'miles': 6.48633, 'factor': 0.8813382210529348}, {'miles': 1.27698, 'factor': 0.9719863396334021}, {'miles': 16.715, 'factor': 0.9232781407097753},
+                 {'miles': 19.3983, 'factor': 0.9833614243304091}, {'miles': 3.89577, 'factor': 0.9121841228586717}, {'miles': 3.92833, 'factor': 0.9994538626794632},
+                 {'miles': 3.84592, 'factor': 0.9048143040668307}, {'miles': 1.64407, 'factor': 0.8743246712664462}, {'miles': 4.08059, 'factor': 0.9019872174989512},
+                 {'miles': 3.83296, 'factor': 0.8885852782527808}]
+
 data = [i for i in data if i['miles'] < 70]  # remove 2 outliers for now
-plt.scatter([i['miles'] for i in data], [i['factor'] for i in data], label='EPS factor')
+data_filtered = [i for i in data_filtered if i['miles'] < 70]  # remove 2 outliers for now
+plt.scatter([i['miles'] for i in data], [i['factor'] for i in data], label='EPS factor (raw)')
+plt.scatter([i['miles'] for i in data_filtered], [i['factor'] for i in data_filtered], label='EPS factor (filtered)')
 plt.legend()
 plt.xlabel('route length (in miles)')
 plt.ylabel('EPS factor')
 plt.title('EPS factor plotted from {} routes using linear reg.'.format(len(data)))
 plt.show()
+
+data = [i['factor'] for i in data]
+data_filtered = [i['factor'] for i in data_filtered]
+
+print(f'Raw data:      mean: {np.mean(data).round(4)}, std.: {np.std(data).round(4)}')
+print(f'Filtered data: mean: {np.mean(data_filtered).round(4)}, std.: {np.std(data_filtered).round(4)}')
--- selfdrive/debug/toyota_eps_factor.py
+++ selfdrive/debug/toyota_eps_factor.py
@@ -2,21 +2,21 @@
 import sys
 import numpy as np
 import matplotlib.pyplot as plt
-from sklearn import linear_model # pylint: disable=import-error
+from sklearn import linear_model  # pylint: disable=import-error
 
 from tools.lib.route import Route
 from tools.lib.logreader import MultiLogIterator
 
+MIN_SAMPLES = 15 * 100
 
-MIN_SAMPLES = 30*100
 
 def to_signed(n, bits):
   if n >= (1 << max((bits - 1), 0)):
     n = n - (1 << max(bits, 0))
   return n
 
-def get_eps_factor(lr, plot=False):
 
+def get_eps_factor(lr, plot=False):
   engaged = False
   torque_cmd, eps_torque = None, None
   cmds, eps = [], []
@@ -33,6 +33,7 @@ def get_eps_factor(lr, plot=False):
         eps_torque = to_signed((m.dat[5] << 8) | m.dat[6], 16)
 
     if engaged and torque_cmd is not None and eps_torque is not None:
+      # if abs(eps_torque - torque_cmd) <= 100:  # assume current factor is close enough
       cmds.append(torque_cmd)
       eps.append(eps_torque)
     else:
@@ -45,16 +46,28 @@ def get_eps_factor(lr, plot=False):
 
   lm = linear_model.LinearRegression(fit_intercept=False)
   lm.fit(np.array(cmds).reshape(-1, 1), eps)
-  scale_factor = 1./lm.coef_[0]
+  scale_factor = 1. / lm.coef_[0]
 
   if plot:
-    plt.plot(np.array(eps)*scale_factor)
+    plt.plot(np.array(eps) * scale_factor)
     plt.plot(cmds)
     plt.show()
   return scale_factor
 
+
 if __name__ == "__main__":
-  r = Route(sys.argv[1])
-  lr = MultiLogIterator(r.log_paths(), wraparound=False)
-  n = get_eps_factor(lr, plot="--plot" in sys.argv)
-  print("EPS torque factor: ", n)
+  # to_use = ['14431dbeedbf3558%7C2020-10-19--18-40-06', '14431dbeedbf3558%7C2020-10-17--16-31-47', '14431dbeedbf3558%7C2020-10-19--17-43-38', '14431dbeedbf3558%7C2020-10-16--17-31-39',
+  #           '14431dbeedbf3558%7C2020-10-19--12-35-29', 'e010b634f3d65cdb%7C2020-06-10--20-07-25', '14431dbeedbf3558%7C2020-10-19--22-55-58', '14431dbeedbf3558%7C2020-10-19--13-40-57',
+  #           '14431dbeedbf3558%7C2020-10-19--14-18-32', '14431dbeedbf3558%7C2020-10-19--22-15-27', '14431dbeedbf3558%7C2020-10-19--12-16-53', '14431dbeedbf3558%7C2020-10-17--21-16-10',
+  #           '14431dbeedbf3558%7C2020-10-17--13-32-42', '14431dbeedbf3558%7C2020-10-19--18-33-54', '14431dbeedbf3558%7C2020-10-19--15-23-23', '14431dbeedbf3558%7C2020-10-17--11-14-47']
+  to_use = ['a0defbc23933e4f8%7C2020-10-21--20-52-46', 'a0defbc23933e4f8%7C2020-10-21--20-59-45']
+
+  for route in to_use:
+    print('working on route: {}'.format(route))
+    r = Route(route)
+    lr = MultiLogIterator(r.log_paths(), wraparound=False)
+    try:
+      n = get_eps_factor(lr, plot="--plot" in sys.argv)
+      print("\n{}: EPS torque factor: {}\n".format(route, n))
+    except Exception as e:
+      print(repr(e))
